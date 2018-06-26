aba4300b956540ac9cf8c97a5e09ffaf5b26d3a2
Kalela <33759757+Kalela@users.noreply.github.com>
2018-06-26 11:14:24 +0300

    [chore] Initialise android app

---
+++ app/src/main/java/com/andela/philskiiiwalker/levelup/MainActivity.java
@@ -0,0 +1,13 @@
+package com.andela.philskiiiwalker.levelup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
