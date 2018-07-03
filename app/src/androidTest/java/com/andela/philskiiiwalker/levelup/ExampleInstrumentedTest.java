cb19e21e4060c1e432af4693934b4e1cbe574778
Kalela <33759757+Kalela@users.noreply.github.com>
2018-07-03 15:39:38 +0300

    kdhfl

--- app/src/androidTest/java/com/andela/philskiiiwalker/levelup/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/andela/philskiiiwalker/levelup/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.andela.philskiiiwalker.levep", appContext.getPackageName());
+        assertEquals("com.andela.philskiiiwalker.levelp", appContext.getPackageName());
     }
 }
