75cc5777b808327aa0b9bd0825bc149985c37995
Kalela <33759757+Kalela@users.noreply.github.com>
2018-07-13 15:17:27 +0300

    [feature] set up share button and share intent

--- app/src/main/java/com/andela/philskiiiwalker/levelup/github_user_profile/view/DisplayDetailsActivity.java
+++ app/src/main/java/com/andela/philskiiiwalker/levelup/github_user_profile/view/DisplayDetailsActivity.java
@@ -1,7 +1,11 @@
 package com.andela.philskiiiwalker.levelup.github_user_profile.view;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -10,6 +14,8 @@ import com.bumptech.glide.Glide;
 
 
 public class DisplayDetailsActivity extends AppCompatActivity {
+
+    Button shareButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,12 +35,34 @@ public class DisplayDetailsActivity extends AppCompatActivity {
     private void setActivityResources(String imageUrl, String devName) {
         TextView developerName = findViewById(R.id.developerNameTextView);
         ImageView devImage = findViewById(R.id.developerImageImageView);
+        shareButton = findViewById(R.id.shareButton);
 
         developerName.setText(devName);
         Glide.with(this)
                 .asBitmap()
                 .load(imageUrl)
                 .into(devImage);
+
+        shareUserProfile(imageUrl, devName);
+    }
+
+    private void shareUserProfile(final String imageUrl, final String devName) {
+
+        shareButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = devName;
+                Uri pictureUri = Uri.parse(imageUrl);
+                Intent shareIntent = new Intent();
+                shareIntent.setAction(Intent.ACTION_SEND);
+                shareIntent.putExtra(Intent.EXTRA_TEXT, username);
+                shareIntent.putExtra(Intent.EXTRA_STREAM, pictureUri);
+                shareIntent.setType("image/*");
+                shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                startActivity(Intent.createChooser(shareIntent, "Share profile"));
+            }
+        });
+
     }
 
 }
